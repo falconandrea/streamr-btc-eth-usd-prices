06b3582e101e9a9d8b37aa0c7ea7d1dd052c02fb
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-08-22 17:56:10 +0200

    feat: new stream script

---
+++ create-new-streamr.js
@@ -0,0 +1,25 @@
+// Import dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Import the Streamr client
+const StreamrClient = require("streamr-client");
+
+// Create async function to create a new stream
+async function createStream() {
+  // Initialize the client with an Ethereum account
+  const streamr = new StreamrClient({
+    auth: {
+      privateKey: process.env.PRIVATE_KEY,
+    },
+  });
+
+  // Requires MATIC tokens (Polygon blockchain gas token)
+  const stream = await streamr.createStream({
+    id: "/btc-eth-usd-price",
+  });
+
+  console.log(stream.id); // e.g. `0x123.../foo/bar`
+}
+
+createStream();
